27e89f110180530be22b2237994982c97ba92f47
Nicholas W. Mazzei <nicholaswmazzei@gmail.com>
2020-01-18 01:23:07 +0000

    Chamadas da API e refatoracao

--- web/src/App.js
+++ web/src/App.js
@@ -1,84 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './style/global.css'
 import './style/App.css'
 import './style/Sidebar.css'
 import './style/Main.css'
+import api from './services/api'
+
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
 
 function App() {
+    const [devs, setDevs] = useState([])
+
+    async function handleAddDev(data){
+        const response = await api.post('/devs', data)
+        setDevs([...devs, response.data])
+    }
+
+    useEffect(()=>{
+        async function loadDevs(){
+            const response = await api.get('/devs')
+            setDevs(response.data)
+        }
+        loadDevs()
+    },[])
+
     return (
         <div id="app">
             <aside>
                 <strong>Cadastrar</strong>
-                <form>
-                    <div className="input-block">
-                        <label htmlFor="github_username">Usuario do Github</label>
-                        <input name="github_username" id="username_github" required/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="techs">Tecnologias</label>
-                        <input name="techs" id="techs" required/>
-                    </div>
-                        
-                    <div className="input-group">
-                        <div className="input-block">
-                            <label htmlFor="latitude">Latitude</label>
-                            <input name="latitude" id="latitude" required/>
-                        </div>
-                        <div className="input-block">
-                            <label htmlFor="longitude">Longitude</label>
-                            <input name="longitude" id="longitude" required/>
-                        </div>
-                    </div>
-                    <button type="submit">Salvar</button>
-
-                </form>
+                <DevForm onSubmit={handleAddDev} />
             </aside>
             <main>
                 <ul>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars0.githubusercontent.com/u/39601848?v=4" alt="Nicholas"/>
-                            <div className="user-info">
-                                <strong>NicholasWM</strong>
-                                <spam>ReactJS, React Native, Flask, NodeJS</spam>
-                            </div>
-                        </header>
-                        <p>Um entusiasta entusiasmado :D</p>
-                        <a href="https://github.com/nicholaswm">Acessar perfil no Github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars0.githubusercontent.com/u/39601848?v=4" alt="Nicholas"/>
-                            <div className="user-info">
-                                <strong>NicholasWM</strong>
-                                <spam>ReactJS, React Native, Flask, NodeJS</spam>
-                            </div>
-                        </header>
-                        <p>Um entusiasta entusiasmado :D</p>
-                        <a href="https://github.com/nicholaswm">Acessar perfil no Github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars0.githubusercontent.com/u/39601848?v=4" alt="Nicholas"/>
-                            <div className="user-info">
-                                <strong>NicholasWM</strong>
-                                <spam>ReactJS, React Native, Flask, NodeJS</spam>
-                            </div>
-                        </header>
-                        <p>Um entusiasta entusiasmado :D</p>
-                        <a href="https://github.com/nicholaswm">Acessar perfil no Github</a>
-                    </li>
-                    <li className="dev-item">
-                        <header>
-                            <img src="https://avatars0.githubusercontent.com/u/39601848?v=4" alt="Nicholas"/>
-                            <div className="user-info">
-                                <strong>NicholasWM</strong>
-                                <spam>ReactJS, React Native, Flask, NodeJS</spam>
-                            </div>
-                        </header>
-                        <p>Um entusiasta entusiasmado :D</p>
-                        <a href="https://github.com/nicholaswm">Acessar perfil no Github</a>
-                    </li>
+                    {devs.map(dev => (
+                        <DevItem key={dev._id} dev={dev}/>
+                    ))}
                 </ul>
             </main>
         </div>
---
+++ web/src/services/api.js
@@ -0,0 +1,6 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'https://3333-faf1a301-71e0-49e3-893b-169419f076fb.ws-us02.gitpod.io/'
+})
+export default api
